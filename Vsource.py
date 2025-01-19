2dcb1ea07b9038819e99b1aab8a30630e73c160f
jjd53 <jjd53@pitt.edu>
2025-01-19 14:27:49 -0500

    First attempt at Solution class and main script

--- Vsource.py
+++ Vsource.py
@@ -1,8 +1,10 @@
+from Bus import Bus
 class Vsource:
     def __init__(self, name,bus1,v):
         self.name = name
         self.bus1 = bus1
         self.v = v
+       #Bus.set_bus(self.bus1,self.v)
 
     def __repr__(self):
         return f"Vsource(name='{self.name}', bus1='{self.bus1}', voltage={self.v})"
